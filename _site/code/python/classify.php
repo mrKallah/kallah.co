41424104ad351a518044fee65028992c52cbf50c
mrKallah <lasse_sortland@hotmail.com>
2018-05-07 19:00:24 +0100

    classifier update
    + Fixed some user interface problems
    - removed the python script
    + added the classifier to the site

---
+++ _site/code/python/classify.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head> <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+  	<title> Classify! </title>
+  </head>
+  <body>
+    <h1> Hello World!</h1>
+    <?php 
+		shell_exec("classify.py"); 
+	?>
+  </body>
+</html>
