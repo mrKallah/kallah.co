20b49795ad8a17fcf04e1ec42a3ece533fcaeceb
mrKallah <lasse_sortland@hotmail.com>
2018-05-07 19:53:00 +0100

    updated to print error

--- _site/code/python/classify.php
+++ _site/code/python/classify.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
   <head> <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
@@ -6,10 +5,11 @@
   </head>
   <body>
     <h1> Hello World!</h1>
+    <img src="img/illustration/layers.png " alt="Neuron" width="80%" height="auto" >
     <?php
-        $output = exec("/var/www/kallah.co/public_html/code/python/classify.py 2>&1");
-        echo $output
-        //echo 'Current PHP version: ' . phpversion();
+                $output = exec("/var/www/kallah.co/public_html/code/python/classify.py 2>&1");
+                echo $output
         ?>
   </body>
 </html>
+
